faaa057552c8d17861fcdfd1811482ca9b17ffbf
notdiogo <notdiogo@users.noreply.github.com>
2026-03-12 16:48:46 +0000

    deploy: 3a1f1e1ce2261d2b5d7fe2848e2bf70115a31acb

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/SideA/sw.js', { scope: '/SideA/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
